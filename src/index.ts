277bac5d582c5627fc6873204a662b2220cac570
Endri Dollani <endridollani99@gmail.com>
2023-11-23 23:37:50 +0100

    Connected to MongoDB Atlas

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import http from "http";
 import cors from "cors";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -20,4 +21,20 @@ const server = http.createServer(app);
 
 server.listen(process.env.PORT, () =>
   console.log("Server is running in port: " + process.env.PORT),
+);
+
+mongoose.Promise = Promise;
+
+mongoose.connect(process.env.DB_CONNECTION_STRING as string);
+
+mongoose.connection.on("connected", () =>
+  console.log("MongoDB Atlas connected successfully!"),
+);
+
+mongoose.connection.on("disconnected", () =>
+  console.log("MongoDB Atlas disconnected."),
+);
+
+mongoose.connection.on("error", (error: Error) =>
+  console.log(`MongoDB Atlas connection error: ${error}`),
 );
