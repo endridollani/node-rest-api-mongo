fd3bdb85b32b37f52f1d1e7050f0998077f62d19
Endri Dollani <endridollani99@gmail.com>
2023-11-23 23:09:56 +0100

    Installed missing packages and startet server running on port 8080

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("App Index");
+import express from "express";
+import http from "http";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  }),
+);
+
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(process.env.PORT, () =>
+  console.log("Server is running in port: " + process.env.PORT),
+);
